121ee43a3190d65aff0beff356b925333c8a3f25
aospina001 <aospina001@gmail.com>
2020-10-06 01:10:49 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
-/* eslint-disable */
-
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
-import "../style/index.scss";
+import "../style/index.css";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  
+    var numbers = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"];
+    var suits = ["&#9824","&#9827","&#9829","&#9830"];
+
+    //numbers [Math.floor(Math.random() * numbers.length)];
+    console.log(suits[Math.floor(Math.random() * suits.length)]);
+    
 };
